420bd04a5bcc4e603a43294ae3fe6982ebdb8c95
Japneet Rajput <japneetrajput2804@gmail.com>
2022-12-04 17:46:57 +0530

    Final commit

---
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.memeshare
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+
+    var currentImageUrl:String? =null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadMeme()
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type= "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT,"Hey! I found this cool meme on Reddit $currentImageUrl")
+            val chooser = Intent.createChooser(intent,"Share this meme using...")
+            startActivity(chooser)
+        }
+        nextButton.setOnClickListener {
+            loadMeme()
+        }
+    }
+
+    fun loadMeme(){
+        progressBar.visibility = View.VISIBLE
+        val apiUrl = "https://meme-api.com/gimme"
+
+//         Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, apiUrl,
+            { response ->
+                // Display the first 500 characters of the response string.
+                val responseObject = JSONObject(response)
+
+                currentImageUrl = responseObject.getString("url")
+                Glide.with(this).load(currentImageUrl).listener(object: RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                }).into(imageView)
+            },
+            { })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
+    }
+}
+
